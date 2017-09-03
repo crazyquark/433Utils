bd75c8d3570d72e175dab2fa7fd84920463ba026
Cristian Sandu <cristian.sandu@gmail.com>
2017-09-03 16:16:17 +0300

    add py wrapper

--- RPi_utils/codesend.py
+++ RPi_utils/codesend.py
@@ -1,1 +1,17 @@
-from ctypes import cdll, c_char_p
+from ctypes import cdll, c_int
+
+def main():
+    ''' Entrypoint '''
+    send = None
+    try:
+        lib = cdll.LoadLibrary('./libcodesend.so')
+        send = lib.send
+        send.restype = c_int
+    except:
+        print 'Not running on target host'
+        return
+
+        print send
+
+if __name__ == '__main__':
+    main()
